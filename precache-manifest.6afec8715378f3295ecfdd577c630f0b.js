f85693c7af6c9407b7bf511ccec8ee10638af8fd
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-08-22 16:37:44 +0200

    Updates

---
+++ precache-manifest.6afec8715378f3295ecfdd577c630f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6124dc70da9bc47d83137e869cddd4c2",
+    "url": "/React-todo-app/index.html"
+  },
+  {
+    "revision": "1281b2d312c660e31989",
+    "url": "/React-todo-app/static/css/main.be4a0b63.chunk.css"
+  },
+  {
+    "revision": "000f2a2a348ff49a1e9a",
+    "url": "/React-todo-app/static/js/2.3ce675d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-app/static/js/2.3ce675d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1281b2d312c660e31989",
+    "url": "/React-todo-app/static/js/main.e9fc964a.chunk.js"
+  },
+  {
+    "revision": "46bbd39ee8ef4e79d9ec",
+    "url": "/React-todo-app/static/js/runtime-main.d74b7e02.js"
+  }
+]);
